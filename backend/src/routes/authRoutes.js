2b0b01dc05d6cf3f6fa37aee76ceba16a8e367e5
DhruvPekhale <dnpekhale@gmail.com>
2025-07-27 15:48:16 +0530

    removed all files

--- backend/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// src/routes/authRoutes.js
-import express from 'express';
-import { registerUser } from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/register', registerUser);
-
-export default router;
